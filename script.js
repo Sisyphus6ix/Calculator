0057669ad913a43215fd3de3090732ab497ffed4
Ryan Silvera <ryan.silvera@icloud.com>
2022-11-01 18:35:48 -0400

    basic JS for the calculator

---
+++ script.js
@@ -0,0 +1,24 @@
+const add = (a,b) => a + b;
+console.log(add(5,10))
+
+const subtract = (a,b) => a - b;
+console.log(subtract(20,10))
+
+const multiply = (a,b) => a * b;
+console.log(multiply(3,3))
+
+const divide = (a, b) => a / b;
+console.log(divide(100,5))
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+      case "+":
+        return add(num1, num2);
+      case "-":
+        return subtract(num1, num2);
+      case "*":
+        return multiply(num1, num2);
+      case "/":
+        return divide(num1, num2);
+    }
+  };
