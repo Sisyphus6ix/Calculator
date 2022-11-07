ea9d5d62c58eaea0c63f37706892777150b7d75d
Ryan Silvera <ryan.silvera@icloud.com>
2022-11-07 18:20:16 -0500

    Created functions for storing number2 and then choosing the operator to apply to num1 and num2

--- script.js
+++ script.js
@@ -8,30 +8,31 @@ const multiplyKey = document.getElementsByClassName('btn-operator')[0]
 const divideKey = document.getElementsByClassName('btn-operator')[4]
 const calculate = document.getElementsByClassName('btn-operator')[3]
 
-const add = (a,b) => a + b;
+const add = (a, b) => parseFloat(a) + parseFloat(b);
 // console.log(add(5,10))
 
-const subtract = (a,b) => a - b;
+const subtract = (a, b) => parseFloat(a) - parseFloat(b);
 // console.log(subtract(20,10))
 
-const multiply = (a,b) => a * b;
+const multiply = (a, b) => parseFloat(a) * parseFloat(b);
 // console.log(multiply(3,3))
 
-const divide = (a, b) => a / b;
+const divide = (a, b) => parseFloat(a) / parseFloat(b);
 // console.log(divide(100,5))
 
 function operate(num1, num2, operator) {
     switch (operator) {
-      case "+":
+      case '+':
         return add(num1, num2);
-      case "-":
+      case '-':
         return subtract(num1, num2);
-      case "*":
+      case '*':
         return multiply(num1, num2);
-      case "/":
+      case '/':
         return divide(num1, num2);
     }
 };
+
 // Giving the user the default value of an empty string
 let userInput = ''
 
@@ -42,20 +43,46 @@ const chosenNum = (e) => {
   if (userInput[0] === '0'){
     userInput = ''
   }
+  if (userInput === '.' && display.innerHTML.indexOf('.') > -1) {
+    userInput = '';
+  }
 
-  const num1 = Number(userInput)
-
+  const chosenNumInput = userInput
+  
   if (userInput.length > 8){
     // do nothing (8 digit limit)
   } else {
-   display.innerHTML = num1
+   display.textContent = Number(chosenNumInput)
   }
 }
+
+const operation = (e) => {
+    num1 = display.textContent
+    parseInt(num1)
+    console.log(`this is num1: ${num1}`)
+    operator = e.target.textContent
+    console.log(operator)
+    userInput = ''
+}
+
+ const result = () => {
+    num2 = display.textContent
+    parseInt(num2)
+    console.log(`this is num2: ${num2}`)
+    operatorTest = operate(num1, num2, operator)
+    console.log(operatorTest)
+    display.textContent = operatorTest
+    userInput = ''
+}
+
+
 
 // Clearing displaying
 const clearDisplay = () => {
   userInput = ''
   display.textContent = '0'
+  num1 = false
+  num2 = false
 }
 
 const test = () => {
@@ -69,8 +96,8 @@ for (num of numKeys) {
 
 // EventListeners
 clear.addEventListener('click', clearDisplay)
-addKey.addEventListener('click', test)
-subtractKey.addEventListener('click', test)
-multiplyKey.addEventListener('click', test)
-divideKey.addEventListener('click', test)
-calculate.addEventListener('click', test)
+addKey.addEventListener('click', operation)
+subtractKey.addEventListener('click', operation)
+multiplyKey.addEventListener('click', operation)
+divideKey.addEventListener('click', operation)
+calculate.addEventListener('click', result)
